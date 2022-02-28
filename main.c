0e7ef83c6dae7380c1c63ab20e6769899b067e83
ngitian <yitianhuang@cpp.edu>
2022-02-28 14:09:11 -0800

    Add program outline

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Yitian Huang, system programming, 02/28/2022
+print menu, read and run different inputs
+1-start a game
+    prompt user input, if correct, go back to menu, else
+    tell user too high or too low. if enter q, return to menu.
+    keep track of nubmer of guesses.
+2-change max number
+3-quit
+    thanks user for playing
+    print out number of guesses required to win
+    if quit game, indicate lost game
+    end game
+*/
